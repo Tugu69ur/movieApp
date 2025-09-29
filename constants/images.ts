316f3f89cc6a78b22c2faff6a0c92e5092cad870
tugu <tugit8833@gmail.com>
2025-09-29 10:47:36 +0800

    a

--- constants/images.ts
+++ constants/images.ts
@@ -1,9 +1,11 @@
 import bg from "@/assets/images/bg.png";
 import highlight from "@/assets/images/highlight.png";
 import rankingGradient from "@/assets/images/rankingGradient.png";
+import weather from "@/assets/images/weather.png";
 
 export const images = {
   bg,
   highlight,
   rankingGradient,
+  weather,
 };
