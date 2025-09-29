78ce3f3ee133129681fe373064ab4e0bf8e6069a
Tugu69ur <149983686+Tugu69ur@users.noreply.github.com>
2025-09-29 09:08:56 +0800

    a

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,19 @@
+import { AuthProvider } from "@/contexts/AuthContext";
 import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "./global.css";
 
 export default function RootLayout() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
-      </Stack>
-    </GestureHandlerRootView>
+    <AuthProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="login" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+        </Stack>
+      </GestureHandlerRootView>
+    </AuthProvider>
   );
 }
